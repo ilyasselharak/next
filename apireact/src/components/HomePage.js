fd8170d58a004752bc6838651389d9e67f4f43ca
ilyass <ilyasselharak@gmail.com>
2022-12-02 23:54:50 +0100

    create api

--- apireact/src/components/HomePage.js
+++ apireact/src/components/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Search from './Search'
+
 function HomePage() {
   return (
     <div>
         <Search/>
         <h1>Main Characters</h1>
         <div className='characters-container'>
-
+         
         </div>
     </div>
   )
