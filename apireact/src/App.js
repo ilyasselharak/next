c912d816faf875fcb1d69efaa85445d7c35cf68a
ilyass <ilyasselharak@gmail.com>
2022-12-04 23:26:19 +0100

    got problem

--- apireact/src/App.js
+++ apireact/src/App.js
@@ -8,13 +8,12 @@ import axios from 'axios';
 
 function App() {
   const [character,setcharacter]=useState([])
-  const [image,setimage]=useState([])
+
   useEffect(()=>{
     axios.get(`https://rickandmortyapi.com/api/character`)
 .then(response=>{
   
   setcharacter(response.data.results[0].name)
-  setimage(response.data.results[0].image)
 
 })
 .catch(err=>{console.log(err)})
@@ -25,7 +24,7 @@ function App() {
       <Header />
       <Routes>
         <Route element={<About/>} path='/about'/>
-        <Route path="/" element={<HomePage imgUrl={image} name={character}/>}/>
+        <Route path="/" element={<HomePage character={character}/>}/>
       </Routes>
 
     </BrowserRouter>
--- apireact/src/components/HomePage.js
+++ apireact/src/components/HomePage.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Search from './Search'
 
-function HomePage(props) {
+function HomePage({character}) {
   return (
     <div>
         <Search/>
         <h1>Main Characters</h1>
         <div className='characters-container'>
           <div>
-            <img src={props.imgUrl}/>
-            <p>{props.name}</p>
+            {
+             character.map((item)=>{
+              return <p>{item.character}</p>
+             })
+            }
+            
           </div>
         </div>
     </div>
